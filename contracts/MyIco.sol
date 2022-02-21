3a657e86ee8a68669c382baf7db4582781840fb5
genialtechie <genialtechie@gmail.com>
2022-02-21 01:06:30 -0500

    standard ERC20 Token

--- contracts/MyIco.sol
+++ contracts/MyIco.sol
@@ -8,8 +8,10 @@ contract MyIco {
   uint public totalSupply;
   //Transfer event 
   event Transfer(address indexed _from, address indexed _to, uint _amount);
+  event Approval(address indexed owner, address indexed spender, uint amount);
   //returns the address' token balance
   mapping(address => uint) public balanceOf; 
+  mapping(address => mapping(address => uint)) public allowance;
 
   constructor(uint256 initSupply) {
     //we set the total supply when contract is created
@@ -28,4 +30,25 @@ contract MyIco {
     emit Transfer(msg.sender, to, amount);
     return true;
   }
+
+  function approve(address spender, uint amount) public returns (bool success) {
+    allowance[msg.sender][spender] = amount;
+    emit Approval(msg.sender, spender, amount);
+    return true;
+  }
+
+  function transferFrom(address from, address to, uint amount) public returns(bool success){
+    //check if caller has enough eth
+    require(balanceOf[from] >= amount );
+    require(allowance[from][msg.sender] >= amount);
+    //transfer balance
+    balanceOf[from] -= amount;
+    balanceOf[to] += amount;
+    //update allowance
+    allowance[from][msg.sender] -= amount;    
+    //emit transfer event 
+    emit Transfer(from, to, amount);
+    return true;
+  }
+
 }
--- test/MyIco.js
+++ test/MyIco.js
@@ -44,12 +44,53 @@ contract('My Ico', function(accounts){
             //transfers 100 tokens from account[0] to [1]
             const transfer = await instance.transfer(accounts[1], 100, {from: accounts[0]});
             //log receipt to console
-            console.log(`You just sent ${transfer.logs[0].args._amount} HBO from ${transfer.logs[0].args._from} to ${transfer.logs[0].args._to}`);
+            console.log(`You just sent ${transfer.logs[0].args._amount}HBO from ${transfer.logs[0].args._from} to ${transfer.logs[0].args._to}`);
             //check balances
             const balance1 = await instance.balanceOf(accounts[1]);
             assert.equal(balance1, 100);
             const balance2 = await instance.balanceOf(accounts[0]);
             assert.equal(balance2, 900);
+        }
+    });
+    it('test approve and allowance', async function(){
+        //test if it returns bool
+        const success = await instance.approve.call(accounts[1], 100);
+        assert.equal(success, true, 'boolean is returned');
+
+        const approve = await instance.approve(accounts[1], 100, {from: accounts[0]});
+        //log receipt to console
+        console.log(`You just approved ${approve.logs[0].args.amount}HBO to ${approve.logs[0].args.spender}`);
+
+        const allowance = await instance.allowance(accounts[0], accounts[1]);
+        assert.equal(allowance, 100);
+    });
+    it('test transfer function ', async function(){
+        try{
+            await instance.approve(accounts[1], 10);
+            //test exception by sending more coins than approved
+            await instance.transferFrom.call(accounts[0], accounts[2], 10000, {from: accounts[1]});
+        } 
+        catch(error) {
+            //handle exception and check if error contains revert
+            assert.fail;
+            assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
+            //check if call returns a boolean
+            const success = await instance.transferFrom.call(accounts[0], accounts[2], 10, {from: accounts[1]});
+            assert.equal(success, true, 'boolean is returned');
+        }
+        finally {
+            //transfers 100 tokens from account[0] to [2] using accounts[1]
+            const transfer = await instance.transferFrom(accounts[0], accounts[2], 10, {from: accounts[1]});
+            //log receipt to console
+            console.log(`You just approved and sent ${transfer.logs[0].args._amount}HBO from ${transfer.logs[0].args._from} to ${transfer.logs[0].args._to}`);
+            //check balances
+            const balance1 = await instance.balanceOf(accounts[0]);
+            assert.equal(balance1, 890);
+            const balance2 = await instance.balanceOf(accounts[2]);
+            assert.equal(balance2, 10);
+            //check allowance
+            const allowance = await instance.allowance(accounts[0], accounts[1]);
+            assert.equal(allowance, 0);
         }
     });
 });
